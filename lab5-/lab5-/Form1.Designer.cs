94a8fce46a66cf2bf8b92ced5e0d44f9d5513238
Darkhanova <91181096+Darkhanova@users.noreply.github.com>
2021-11-28 19:10:13 +0800

    Add files via upload

---
+++ lab5-/lab5-/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace Lab5_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pbMain = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.txtlog = new System.Windows.Forms.RichTextBox();
+            this.counts = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbMain
+            // 
+            this.pbMain.Location = new System.Drawing.Point(15, 16);
+            this.pbMain.Margin = new System.Windows.Forms.Padding(4);
+            this.pbMain.Name = "pbMain";
+            this.pbMain.Size = new System.Drawing.Size(801, 568);
+            this.pbMain.TabIndex = 0;
+            this.pbMain.TabStop = false;
+            this.pbMain.Paint += new System.Windows.Forms.PaintEventHandler(this.pbMain_Paint);
+            this.pbMain.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pbMain_MouseClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 30;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // txtlog
+            // 
+            this.txtlog.Location = new System.Drawing.Point(824, 16);
+            this.txtlog.Margin = new System.Windows.Forms.Padding(4);
+            this.txtlog.Name = "txtlog";
+            this.txtlog.ReadOnly = true;
+            this.txtlog.Size = new System.Drawing.Size(188, 567);
+            this.txtlog.TabIndex = 1;
+            this.txtlog.Text = "";
+            this.txtlog.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtlog_KeyDown);
+            // 
+            // counts
+            // 
+            this.counts.AutoSize = true;
+            this.counts.Location = new System.Drawing.Point(725, 20);
+            this.counts.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.counts.Name = "counts";
+            this.counts.Size = new System.Drawing.Size(55, 20);
+            this.counts.TabIndex = 2;
+            this.counts.Text = "Очки: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1029, 600);
+            this.Controls.Add(this.counts);
+            this.Controls.Add(this.txtlog);
+            this.Controls.Add(this.pbMain);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Обработка событий";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbMain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.PictureBox pbMain;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RichTextBox txtlog;
+        private System.Windows.Forms.Label counts;
+    }
+}
+
---
+++ lab5-/lab5-/Form1.cs
@@ -0,0 +1,185 @@
+﻿using Lab5_.Objects;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5_
+{
+    public partial class Form1 : Form
+    {
+        List<BaseObject> objects = new List<BaseObject>();
+        Player player;
+        Marker marker;
+        DeathZone death;       
+        Apple apple;
+        Apple apple2;
+        Random rand = new Random();
+        int count = 0;
+       
+        public Form1()
+        {
+            InitializeComponent();
+            
+            player = new Player(pbMain.Width / 2, pbMain.Height / 2, 0);
+            player.OnOverlap += (p, obj) =>
+            {
+                txtlog.Text = $"[{DateTime.Now:HH:mm:ss:ff}] Игрок пересекся с {obj}\n" + txtlog.Text;
+            };
+
+            // добавил реакцию на пересечение с маркером
+            player.OnMarkerOverlap += (m) =>
+            {
+                objects.Remove(m);
+                marker = null;
+            };
+            player.OnAppleOverlap += (m) =>
+            {
+                
+                count++;
+                objects.Remove(m);
+                apple = null;
+                apple = new Apple(rand.Next(0, pbMain.Width), rand.Next(0, pbMain.Height), 0);
+                objects.Add(apple);
+                if(count == 10)
+                {
+                    objects.Clear();
+                    MessageBox.Show("Ты выиграл!");
+                    Environment.Exit(0);
+                }
+
+            };
+            player.OnZoneOverlap += (m) =>
+            {
+                objects.Remove(m);
+                death = null;
+                death = new DeathZone(rand.Next(0, pbMain.Width), rand.Next(0, pbMain.Height), 0);
+                objects.Add(death);
+                count--;
+            };
+            
+
+
+            
+            marker = new Marker(pbMain.Width / 2 + 50, pbMain.Height / 2 + 50, 0);
+            apple = new Apple(rand.Next(0, pbMain.Width), rand.Next(0, pbMain.Height), 0);
+            apple2 = new Apple(rand.Next(0, pbMain.Width), rand.Next(0, pbMain.Height), 0);
+            
+            death = new DeathZone(rand.Next(0, pbMain.Width), rand.Next(0, pbMain.Height), 0);
+            objects.Add(death);
+            objects.Add(marker);
+            objects.Add(player);
+            objects.Add(apple);
+            objects.Add(apple2);
+            
+        }
+
+        private void pbMain_Paint(object sender, PaintEventArgs e)
+        {
+            counts.Text = "Очки: " + count;
+            var g = e.Graphics;
+
+            g.Clear(Color.White);
+
+            updatePlayer();
+
+            // пересчитываем пересечения
+            foreach (var obj in objects.ToList())
+            {
+                if (obj != player && player.Overlaps(obj, g))
+                {
+                    player.Overlap(obj);
+                    obj.Overlap(player);
+                }
+            }
+
+            // рендерим объекты
+            foreach (var obj in objects)
+            {
+                g.Transform = obj.GetTransform();
+                obj.Render(g);
+            }
+            
+        }
+
+        public void updatePlayer()
+        {
+            if (marker != null)
+            {
+                float dx = marker.X - player.X;
+                float dy = marker.Y - player.Y;
+                float length = (float)Math.Sqrt(dx * dx + dy * dy);
+                dx /= length;
+                dy /= length;
+                player.vX += dx * 0.5f;
+                player.vY += dy * 0.5f;
+                player.Angle = 90 - (float)Math.Atan2(player.vX, player.vY) * 180 / (float)Math.PI;
+            }
+            player.vX += -player.vX * 0.1f;
+            player.vY += -player.vY * 0.1f;
+
+            // пересчет позиция игрока с помощью вектора скорости
+            player.X += player.vX;
+            player.Y += player.vY;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+
+            pbMain.Invalidate();
+        }
+
+        private void pbMain_MouseClick(object sender, MouseEventArgs e)
+        {
+            // тут добавил создание маркера по клику если он еще не создан
+            if (marker == null)
+            {
+                marker = new Marker(0, 0, 0);
+                objects.Add(marker); // и главное не забыть пололжить в objects
+            }
+
+            // а это так и остается
+            marker.X = e.X;
+            marker.Y = e.Y;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtlog_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.W)
+            {
+                player.Angle = 270;
+                player.vY--;
+                Refresh();
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                player.Angle = 90;
+                player.vY++;
+                Refresh();
+            }
+            if (e.KeyCode == Keys.A)
+            {
+                player.Angle = 180;
+                player.vX--;
+                Refresh();
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                player.Angle = 0;
+                player.vX++;
+                Refresh();
+            }
+        }
+    }
+}

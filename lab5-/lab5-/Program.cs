968827e3ba50ead8f8446f86db60034a6474b599
Darkhanova <91181096+Darkhanova@users.noreply.github.com>
2021-12-07 22:00:38 +0800

    Add files via upload

---
+++ lab5-/lab5-/Program.cs
@@ -0,0 +1,23 @@
+﻿using Lab5_;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab5_
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
